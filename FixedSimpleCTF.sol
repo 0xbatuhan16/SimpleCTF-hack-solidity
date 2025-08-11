9be829bb63079ce3c464361b6ff6dc87cd77caa2
Batuhan <157303795+0xbatuhan16@users.noreply.github.com>
2025-08-11 17:56:07 +0300

    Update FixedSimpleCTF.sol

--- FixedSimpleCTF.sol
+++ FixedSimpleCTF.sol
@@ -1,1 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract SimpleCTF {
+    string private flag = "CTF{smart_contracts_are_fun!}";
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function getFlag() public view returns (string memory) {
+        require(msg.sender == owner, "Not the owner!");
+        return flag;
+    }
+
+    function changeOwner(address _newOwner) public {
+        require(msg.sender == owner, "Not the owner!");
+        owner = _newOwner;
+    }
+}
